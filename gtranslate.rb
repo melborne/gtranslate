342ce1eefac86463c6c55e7d7c7a1017a8f198d3
kyoendo <postagie@gmail.com>
2010-11-20 18:22:27 +0900

    add codes method to list translate codes translate method args change to symbol separate parse_blog method to another class

--- gtranslate.rb
+++ gtranslate.rb
@@ -1,130 +1,108 @@
 #!/opt/local/bin/ruby1.9
 #-*-encoding: utf-8-*-
-%w(nokogiri rest_client json cgi open-uri).each { |lib| require lib }
+%w(cgi rest_client json).each { |lib| require lib }
+module Enumerable; alias :reduce :each_with_object end
 
 class GTranslate
-  CSS = {:hatena => ".hatena-body .day .body>.section" }
-  def initialize(api_key, url=nil, host=:hatena)
+  def initialize(api_key)
     @api_key = api_key
-    @url = url
-    @host = host
   end
 
-  def get(range=0..-1, url=nil)
-    raise unless url = @url
-    data = Nokogiri::HTML(open url)
-    parse(data, range)
-  rescue => e
-    raise "Access failed with #{@url}: #{e}"
-  end
-
-  def translate(lines, source='ja', target='en')
+  def translate(lines, opts={:from => :ja, :to => :en})
     l, threads = [], []
     lines.each_with_index do |line, i|
-      url = urlize( CGI.escape(line), source, target )
+      url = urlize(line, opts[:from], opts[:to] )
       threads << Thread.new(url, i) do |site, no|
         res = RestClient.get site
-        raise unless res.code == 200
+        raise RestClient::RequestFailed unless res.code == 200
         l << [no, JSON.parse(res)['data']['translations'][0]['translatedText']]
       end
     end
     threads.each { |th| th.join }
     l.sort_by { |n, _| n }.map(&:last)
-  rescue => e
-    STDERR.puts "Access failed with Google Translate API: #{e}"
+  end
+
+  def codes
+    CODE.lines.reduce({}) do |line, h|
+      country, code = line.strip.split(/\s+/)
+      h[country.intern] = code.intern
+    end
   end
 
   private
-  def parse(data, range)
-    l = []
-    case @host
-    when :hatena
-      data.search(CSS[@host]).each do |node|
-        node.children[range].each do |ch, i|
-          l << ch.text unless ch.text =~ /^\s+$/
-        end
-      end
-    else
-      raise ArgumentError, "wrong host name"
-    end
-    l
-  end
-  
-  def urlize(data, source, target)
+  def urlize(data, from, to)
     "https://www.googleapis.com/language/translate/v2" +
-        "?key=#{@api_key}&q=#{data}&source=#{source}&target=#{target}"
+        "?key=#{@api_key}&q=#{CGI.escape data}&source=#{from.to_s}&target=#{to.to_s}"
   end
+
+CODE =<<EOS
+Afrikaans   af
+Albanian  sq
+Arabic  ar
+Basque  eu
+Belarusian  be
+Bulgarian   bg
+Catalan   ca
+Chinese-Simplified  zh-CN
+Chinese-Traditional   zh-TW
+Croatian  hr
+Czech   cs
+Danish  da
+Dutch   nl
+English   en
+Estonian  et
+Filipino  tl
+Finnish   fi
+French  fr
+Galician  gl
+German  de
+Greek   el
+Haitian-Creole  ht
+Hebrew  iw
+Hindi   hi
+Hungarian   hu
+Icelandic   is
+Indonesian  id
+Irish   ga
+Italian   it
+Japanese  ja
+Latvian   lv
+Lithuanian  lt
+Macedonian  mk
+Malay   ms
+Maltese   mt
+Norwegian   no
+Persian   fa
+Polish  pl
+Portuguese  pt
+Romanian  ro
+Russian   ru
+Serbian   sr
+Slovak  sk
+Slovenian   sl
+Spanish   es
+Swahili   sw
+Swedish   sv
+Thai  th
+Turkish   tr
+Ukrainian   uk
+Vietnamese  vi
+Welsh   cy
+Yiddish   yi
+EOS
 end
 
 if __FILE__ == $0
-  blog = "http://d.hatena.ne.jp/keyesberry/20101116/p1"
   api_key = "AIzaSyBTgpjSu-vy3f1r-tj5wU8M7qkT_5cgYf4"
 
-  tr = GTranslate.new(api_key, blog)
-  original = tr.get(17..18)
-  through = 'ar'
-  english = tr.translate(original, 'ja', through)
-  japanese = tr.translate(english, through, 'ja')
+  tr = GTranslate.new(api_key)
+  p tr.codes
+  # japanese = tr.translate(english, through, 'ja')
 
-  set = [original, english, japanese].map { |l| l.join("\n")}.map { |l| l.split("\n\n") }
-  set.shift.zip(*set).each do |org, en, ja|
-    puts org, "-"*40 , en, "-"*40, ja
-    puts "="*40
-  end
+  # set = [original, english, japanese].map { |l| l.join("\n")}.map { |l| l.split("\n\n") }
+  # set.shift.zip(*set).each do |org, en, ja|
+  #   puts org, "-"*40 , en, "-"*40, ja
+  #   puts "="*40
+  # end
 end
 
-__DATA__
-Language 	code
-Afrikaans 	af
-Albanian 	sq
-Arabic 	ar
-Basque 	eu
-Belarusian 	be
-Bulgarian 	bg
-Catalan 	ca
-Chinese Simplified 	zh-CN
-Chinese Traditional 	zh-TW
-Croatian 	hr
-Czech 	cs
-Danish 	da
-Dutch 	nl
-English 	en
-Estonian 	et
-Filipino 	tl
-Finnish 	fi
-French 	fr
-Galician 	gl
-German 	de
-Greek 	el
-Haitian Creole 	ht
-Hebrew 	iw
-Hindi 	hi
-Hungarian 	hu
-Icelandic 	is
-Indonesian 	id
-Irish 	ga
-Italian 	it
-Japanese 	ja
-Latvian 	lv
-Lithuanian 	lt
-Macedonian 	mk
-Malay 	ms
-Maltese 	mt
-Norwegian 	no
-Persian 	fa
-Polish 	pl
-Portuguese 	pt
-Romanian 	ro
-Russian 	ru
-Serbian 	sr
-Slovak 	sk
-Slovenian 	sl
-Spanish 	es
-Swahili 	sw
-Swedish 	sv
-Thai 	th
-Turkish 	tr
-Ukrainian 	uk
-Vietnamese 	vi
-Welsh 	cy
-Yiddish 	yi
--- tc_gtranslate.rb
+++ tc_gtranslate.rb
@@ -6,20 +6,24 @@ require_relative "gtranslate"
 
 class TestGTranslate < Test::Unit::TestCase
   def setup
-    blog = "http://d.hatena.ne.jp/keyesberry/20101116/p1"
     api_key = "AIzaSyBTgpjSu-vy3f1r-tj5wU8M7qkT_5cgYf4"
-
-    @tr = GTranslate.new(api_key, blog)
+    @gt = GTranslate.new(api_key)
   end
 
-  def test_parse
-    # lines = @tr.parse(0..62)
-    # assert_equal(62, lines.size)
+  def test_translate_a_to_b
+    org = %w(今日は寝坊した やっぱりRubyは最高のプログラミング言語だ 最高のハッカーになるためには、寝ている時間はない。)
+    res = ["Today I rised late.", "Ruby is a greatest programming lauguage.", "To become an extream hacker, you have no time to sleep."]
+    # assert_equal(res, @gt.translate(org, :from => :ja, :to => :en))
   end
- 
-  def test_translate_ja_to_en
-    assert_equal("hello", @tr.translate("こんにちは".split("\n")).join)
-    # english = tr.translate(lines, 'ja', 'en')
-    # japanese = tr.translate(english, 'en', 'ja')
+
+  def test_translate_one_to_many
+    org = ["Today I was overslept"]
+    res = []
+    # assert_equal(res, @gt.translate(org, :from => , :to => []))
   end
+
+  def test_codes
+    assert_equal('expected', @gt.codes)
+  end
+
 end
