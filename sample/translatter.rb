c4492efcd53231b4707d65c5921e12139a99ecaf
kyoendo <postagie@gmail.com>
2010-11-24 14:47:17 +0900

    add translate boomerang say and voices commands to translatter.rb

--- sample/translatter.rb
+++ sample/translatter.rb
@@ -2,30 +2,70 @@
 require "gtranslate"
 
 module Termtter::Client
+  class << self
+    def opt_parse(args, regex)
+      opts = {}
+      args = args.gsub(regex) do
+        key, value = $1, $2
+        opts[key.intern] =
+          case value
+          when /true|false/
+            eval value
+          when /\w+:\w+/
+            value.split(":").map(&:intern)
+          else
+            value.intern
+          end
+        ''
+      end
+      return args, opts
+    end
+
+    def textize(arg)
+      arg =~ /^\d+$/ ? Termtter::API.twitter.show(arg).text : arg
+    end
+
+    def init_translator
+      GTranslate.new(config.google_translate_api)
+    end
+  end
+
   register_command(
     :name => :translate,
-    :help => ['translate TEXT [from:, to:]', 'Language Translation by Google Translate API'],
+    :help => ['translate ID or TEXT [from:, to:]', 'Language Translation by Google Translate API'],
     :exec => lambda { |args|
-      opts = {}
-      args!.gsub(/\s*(from|to):([a-z]{2})\s*/) { opts[$1.intern] = $2.intern ; ''}
-      text =
-        if args =~ /^\d+$/
-          Termtter::API.twitter.show(text).text if text =~ /^\d+$/
-        else
-          args
-        end
-
-      translator = GTranslate.new(config.google_translate_api)
-      result = translator.translate(text, opts)
+      arg, opts = opt_parse(args, /\s*(from|to):([a-z:]+)\s*/)
+      text = textize(arg)
+      result = init_translator.translate(text, opts)
       puts "=> #{result}"
     }
   )
 
   register_command(
     :name => :boomerang,
-    :help => ['boomerang TEXT [through:]', 'Translate to Original Language through Other Languages'],
-    :exec => lambda { |args| 
-      
+    :help => ['boomerang ID or TEXT [through:]', 'Translate to Original Language through Other Languages'],
+    :exec => lambda { |args|
+      arg, opts = opt_parse(args, /\s*(from|through|verbose):([a-z:]+)\s*/)
+      text = textize(arg)
+      result = init_translator.boomerang(text, opts)
+      puts "=> #{result}"
+    }
+  )
+
+  register_command(
+    :name => :say,
+    :help => ['say TEXT [voice:]', 'Speak a Text Out'],
+    :exec => lambda { |args|
+      text, opts = opt_parse(args, /\s*(voice):([a-zA-Z_]+)\s*/)
+      GTranslate.say(text, opts[:voice])
+    }
+  )
+
+  register_command(
+    :name => :voices,
+    :help => ['voice TEXT [voice:]', 'List available VOICES for say command'],
+    :exec => lambda { |args|
+      puts "=> #{GTranslate::VOICES.join(", ")}"
     }
   )
 end
